18fcf93ed6cf832070e03bcd619924dbd7c3c587
Carlos Cortez <crtzrms@gmail.com>
2022-07-29 14:26:38 -0300

    Initial

---
+++ dxgi_proxy/dxgi/dxgi.cpp
@@ -0,0 +1,22 @@
+// dxgi.cpp : Defines the exported functions for the DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "dxgi.h"
+
+
+// This is an example of an exported variable
+DXGI_API int ndxgi=0;
+
+// This is an example of an exported function.
+DXGI_API int fndxgi(void)
+{
+    return 0;
+}
+
+// This is the constructor of a class that has been exported.
+Cdxgi::Cdxgi()
+{
+    return;
+}
